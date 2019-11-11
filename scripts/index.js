899325dffd7fe7891c41608a35d8b6bc060bcf4e
GloriaBorga <47982588+GloriaBorga@users.noreply.github.com>
2019-11-11 18:41:04 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,11 @@ const navSlide = () => {
   const burger = document.querySelector('.hamburger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
+  const hideMenu = document.querySelector('.hide-menu');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    hideMenu.classList.toggle('none');
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
@@ -16,9 +18,23 @@ const navSlide = () => {
 
     burger.classList.toggle('toggle');
   });
+  // Navbar Hide
+  hideMenu.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    hideMenu.classList.toggle('none');
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+      }
+    });
+
+  }
+  )
+
 }
 navSlide();
-
 // Accordion
 
 let acc = document.getElementsByClassName("accordion");
@@ -37,3 +53,8 @@ for(let i=0;i < acc.length; i++){
     }
   }
 }
+
+
+
+
+
